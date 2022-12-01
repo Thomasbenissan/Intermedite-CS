1f4c5d4852cf09c315cab87e8f837b0ab7de5a2e
Thomasbenissan <91895015+Thomasbenissan@users.noreply.github.com>
2022-11-30 23:56:44 -0500

    Add files via upload
    I wasn't able to figure out how to upload it with git using the terminal, so I just dragged the folder in.

---
+++ BuzzFeedQuiz/src/Answer.java
@@ -0,0 +1,11 @@
+//By Thomas Benissan
+public class Answer {
+    String label;
+    int points;
+
+    Answer(String label, int points) {
+        this.label = label;
+        this.points = points;
+
+    }
+}
